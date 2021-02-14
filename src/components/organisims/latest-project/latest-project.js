dbe82aa6d8a66487a134feb53f8d45f15a2c9ec2
Fajrin Mahyuddin <fajrin.telkom12@gmail.com>
2021-02-15 07:26:49 +0800

    grid animation

--- src/components/organisims/latest-project/latest-project.js
+++ src/components/organisims/latest-project/latest-project.js
@@ -1,39 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { Title, Text, Label, Image } from "components";
+import { SpringGrid, layout, CSSGrid } from "react-stonecutter";
 
 const LatestProject = () => {
-  const stack = ["Javascript", "Express", "Grapql"];
+  const stack = [
+    {
+      src:
+        "https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ",
+    },
+    {
+      src:
+        "https://i.picsum.photos/id/101/2621/1747.jpg?hmac=cu15YGotS0gIYdBbR1he5NtBLZAAY6aIY5AbORRAngs",
+    },
+    {
+      src:
+        "https://i.picsum.photos/id/1018/3914/2935.jpg?hmac=3N43cQcvTE8NItexePvXvYBrAoGbRssNMpuvuWlwMKg",
+    },
+    {
+      src:
+        "https://i.picsum.photos/id/1016/3844/2563.jpg?hmac=WEryKFRvTdeae2aUrY-DHscSmZuyYI9jd_-p94stBvc",
+    },
+    {
+      src:
+        "https://i.picsum.photos/id/1016/3844/2563.jpg?hmac=WEryKFRvTdeae2aUrY-DHscSmZuyYI9jd_-p94stBvc",
+    },
+  ];
+  const stack1 = [
+    {
+      src:
+        "https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ",
+    },
+
+    {
+      src:
+        "https://i.picsum.photos/id/1016/3844/2563.jpg?hmac=WEryKFRvTdeae2aUrY-DHscSmZuyYI9jd_-p94stBvc",
+    },
+  ];
+  const stack2 = [
+    {
+      src:
+        "https://i.picsum.photos/id/101/2621/1747.jpg?hmac=cu15YGotS0gIYdBbR1he5NtBLZAAY6aIY5AbORRAngs",
+    },
+    {
+      src:
+        "https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ",
+    },
+    {
+      src:
+        "https://i.picsum.photos/id/1018/3914/2935.jpg?hmac=3N43cQcvTE8NItexePvXvYBrAoGbRssNMpuvuWlwMKg",
+    },
+  ];
+  const [imgs, setImgs] = useState(stack);
   return (
     <div className="container-content p-55-0 text-center" id="project">
       <Title className="text-bold font-large-xx ">Latest Project</Title>
+      <button onClick={() => setImgs(stack)}>satu</button>
+      <button onClick={() => setImgs(stack1)}>dua</button>
+      <button onClick={() => setImgs(stack2)}>tiga</button>
       <div className="image-wrapper">
-        <Image
-          className="img"
-          src="https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ"
-          alt="https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ"
-        />
-        <Image
-          className="img"
-          src="https://i.picsum.photos/id/101/2621/1747.jpg?hmac=cu15YGotS0gIYdBbR1he5NtBLZAAY6aIY5AbORRAngs"
-          alt="https://i.picsum.photos/id/101/2621/1747.jpg?hmac=cu15YGotS0gIYdBbR1he5NtBLZAAY6aIY5AbORRAngs"
-        />
-        <Image
-          className="img"
-          src="https://i.picsum.photos/id/1018/3914/2935.jpg?hmac=3N43cQcvTE8NItexePvXvYBrAoGbRssNMpuvuWlwMKg"
-          alt="https://i.picsum.photos/id/1018/3914/2935.jpg?hmac=3N43cQcvTE8NItexePvXvYBrAoGbRssNMpuvuWlwMKg"
-        />
-        <div className="box img">
-          <span className="date-box">3 Dec</span>
-          {/* <CalendarOutlined className="ico-galery" /> */}
-        </div>
-        <div className="box img">
-          <span className="box-quote">"Make a beauty day with code"</span>
-        </div>
-        <Image
-          className="img"
-          src="https://i.picsum.photos/id/1016/3844/2563.jpg?hmac=WEryKFRvTdeae2aUrY-DHscSmZuyYI9jd_-p94stBvc"
-          alt="https://i.picsum.photos/id/1016/3844/2563.jpg?hmac=WEryKFRvTdeae2aUrY-DHscSmZuyYI9jd_-p94stBvc"
-        />
+        <CSSGrid
+          component="div"
+          columns={3}
+          columnWidth={200}
+          gutterWidth={5}
+          gutterHeight={5}
+          layout={layout.pinterest}
+          duration={500}
+          easing="ease-out"
+          enter={() => ({ translateY: 222 })}
+          entered={() => ({ scale: 1, opacity: 1 })}
+          exit={() => ({ scale: 0, opacity: 0 })}
+        >
+          {imgs.map((item, i) => {
+            return (
+              <div key={i} itemHeight={200}>
+                <Image className="img" src={item.src} alt={item.src} />
+              </div>
+            );
+          })}
+        </CSSGrid>
       </div>
     </div>
   );
