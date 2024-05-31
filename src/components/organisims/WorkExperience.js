57c558078e095b530f60647d17a883d938b378ac
fajrin <fajrin.telkom12@gmail.com>
2024-05-31 10:30:11 +0800

    update work-experience

--- src/components/organisims/WorkExperience.js
+++ src/components/organisims/WorkExperience.js
@@ -6,8 +6,8 @@ import React from "react";
 const experiences = [
   {
     company: "Zodiac Solution, PTE Ltd",
-    period: "Okt 2021 - Preset",
-    position: "Fulltime Remote Frontend Developer",
+    period: "Okt 2021 - May 2024",
+    position: "Fulltime Remote - Frontend Developer",
     color: "#286F6C",
     description:
       "Full-time remote Frontend Developer at a private company in Singapore, responsible for creating the UI for various projects. I use NextJS and Typescript as the main frameworks to ensure the successful completion of our projects.",
@@ -15,7 +15,7 @@ const experiences = [
   {
     company: "Pallaka Studio",
     period: "Sep 2019 - Okt 2021",
-    position: "Fulltime Remote Frontend Developer",
+    position: "Fulltime Remote/Hybrid - Frontend Developer",
     color: "#E6AB35",
     description:
       "Working as a frontend developer, I handle all UI projects. Leveraging Docker, ReactJS, and Javascript as our core technologies, we ensure the successful completion of every project.",
@@ -26,7 +26,7 @@ const experiences = [
     position: "Fullstack Developer",
     color: "#F26440",
     description:
-      "As the full-stack position at this company, my responsibilities span both the backend and frontend even server, along with providing IT support. I utilize PHP and jQuery to successfully complete our projects, taking charge of various tasks across the entire development spectrum.",
+      "As the fullstack position at this company, my responsibilities span both the backend and frontend even server, along with providing IT support. I utilize PHP and jQuery to successfully complete our projects, taking charge of various tasks across the entire development spectrum.",
   },
 ];
 
